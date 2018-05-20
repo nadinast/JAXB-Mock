fa0a4c959ab84f69b2b957a871b888c70f131b28
Nadina Streanga <nadistreanga@gmail.com>
2018-05-20 12:20:27 +0300

    Added XSD to Java conversion

---
+++ src/FileGenerator.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileGenerator {
+    private List<JavaFile> fileList = new ArrayList<>();
+    private int size = 0;
+
+    public void addFile(String className) {
+        fileList.add(new JavaFile(className));
+        size++;
+    }
+
+    public void removeFile() {
+        fileList.get(size - 1).writeFile();
+        fileList.remove(size - 1);
+        size--;
+    }
+
+    public void addAttributes(String type, String name){
+        fileList.get(size - 1).addFields(type, name);
+    }
+}
